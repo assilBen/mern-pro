1565cd12401bf12e4abf228cbee7224b8a877565
Assil Ben mimoun <149869094+assilBen@users.noreply.github.com>
2024-02-06 12:00:22 +0100

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-auth-8f7f7.firebaseapp.com",
+  projectId: "mern-auth-8f7f7",
+  storageBucket: "mern-auth-8f7f7.appspot.com",
+  messagingSenderId: "1008054185285",
+  appId: "1:1008054185285:web:ba62c4344eff2763ae222c",
+  measurementId: "G-3BWRSG8CBD"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
